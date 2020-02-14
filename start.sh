ee27a35b48b44ddd52fcab38266b8e7f052012a9
j.fuessl <j.fuessl@adito.de>
2020-02-14 14:20:07 +0100

    Created Dockerfile, created run.sh, added plugin, added configuration

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-    solr zk upconfig -z localhost:9983 -n adito_config_v2 -d adito_config_v2&
-    solr zk upconfig -z localhost:9983 -n adito_config_v3 -d adito_config_v3&
-    solr zk upconfig -z localhost:9983 -n adito_config_v4 -d adito_config_v4&
+    solr zk upconfig -z localhost:9983 -n adito_config_v2 -d /opt/solr/server/solr/configsets/adito_config_v2&
+    solr zk upconfig -z localhost:9983 -n adito_config_v3 -d /opt/solr/server/solr/configsets/adito_config_v3&
+    solr zk upconfig -z localhost:9983 -n adito_config_v4 -d /opt/solr/server/solr/configsets/adito_config_v4&
 
     solr-foreground -c
