a2b1a413e58f3d7de9fa9c5f6c5f45d6bf58183d
Johannesfu <j.fuessl@adito.de>
2021-02-05 12:37:08 +0100

    add check if container starts as root

--- start.sh
+++ start.sh
@@ -1,7 +1,22 @@
-    solr zk upconfig -z localhost:9983 -n adito_config_v2 -d /opt/solr/server/solr/configsets/adito_config_v2&
-    solr zk upconfig -z localhost:9983 -n adito_config_v3 -d /opt/solr/server/solr/configsets/adito_config_v3&
-    solr zk upconfig -z localhost:9983 -n adito_config_v4 -d /opt/solr/server/solr/configsets/adito_config_v4&
-    solr zk upconfig -z localhost:9983 -n adito_config_v5 -d /opt/solr/server/solr/configsets/adito_config_v5&
-    solr zk upconfig -z localhost:9983 -n adito_config_v6 -d /opt/solr/server/solr/configsets/adito_config_v6&
+#!/bin/bash
+load_config(){
+  echo "Waiting for Zookeeper to launch on 9983"
+  while ! nc localhost 9983; do
+    sleep 4
+    echo "Waiting for Zookeeper to launch on 9983"
+  done
+  solr zk upconfig -z localhost:9983 -n adito_config_v2 -d /opt/solr/server/solr/configsets/adito_config_v2&
+  solr zk upconfig -z localhost:9983 -n adito_config_v3 -d /opt/solr/server/solr/configsets/adito_config_v3&
+  solr zk upconfig -z localhost:9983 -n adito_config_v4 -d /opt/solr/server/solr/configsets/adito_config_v4&
+  solr zk upconfig -z localhost:9983 -n adito_config_v5 -d /opt/solr/server/solr/configsets/adito_config_v5&
+  solr zk upconfig -z localhost:9983 -n adito_config_v6 -d /opt/solr/server/solr/configsets/adito_config_v6&
+}
+load_config&
 
-    solr-foreground -c
+USERID=$(id -u)
+if [ $USERID -eq "0" ]
+then
+gosu solr solr-foreground -c
+else
+solr-foreground -c
+fi
