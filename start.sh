7a9fb3c617d0f6192f0ab92f7e6d2d18b77ba4f6
Johannesfu <j.fuessl@adito.de>
2021-06-29 00:52:02 +0200

    Added Loglevel-Enviroment

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 load_config() {
   echo "Waiting for Zookeeper to launch on 9983"
   while ! nc localhost 9983; do
-    sleep 4
+    sleep 1
     echo "Waiting for Zookeeper to launch on 9983"
   done
   existing_configsets=$(curl http://localhost:8983/solr/admin/configs?action=LIST)
-  echo existing configsets: $existing_configsets
+  echo $existing_configsets
   for d in $( ls /opt/solr/server/solr/configsets/); do
     if ! echo "$existing_configsets" | grep -q $d; then
       echo upload $d
@@ -15,14 +15,18 @@ load_config() {
       echo $d already present
     fi
   done
+  if [ "$LOGLEVEL" != "ALL" ] && [ "$LOGLEVEL" != "TRACE" ] && [ "$LOGLEVEL" != "DEBUG" ] && [ "$LOGLEVEL" != "INFO" ] && [ "$LOGLEVEL" != "WARN" ] && [ "$LOGLEVEL" != "ERROR" ] && [ "$LOGLEVEL" != "FATAL" ] && [ "$LOGLEVEL" != "OFF" ]; then
+    echo LOGLEVEL $LOGLEVEL not valid! Set LOGLEVEL to WARN
+    LOGLEVEL="WARN"
+  fi
+  curl -s http://localhost:8983/solr/admin/info/logging --data-binary "set=root:$LOGLEVEL&wt=json" -o /dev/null
 }
 
 load_config&
 
 USERID=$(id -u)
-if [ $USERID -eq "0" ]
-then
-gosu solr solr-foreground -c
+if [ $USERID -eq "0" ]; then
+  gosu solr solr-foreground -c
 else
-solr-foreground -c
-fi	
+  solr-foreground -c
+fi
